c668f67c9fdbef70fc4e81121310ddfbfbcf0c2f
joekaro <obanovwejoseph@gmail.com>
2026-03-03 00:56:38 +0100

    Remove unused product page

--- frontend/app/product/page.tsx
+++
@@ -1,7 +0,0 @@
-import { getProductById } from "@/lib/api";
-import ProductPageClient from "./ProductPageClient";
-
-export default async function ProductPage() {
-  const product = await getProductById("prod_001");
-  return <ProductPageClient product={product} />;
-}
